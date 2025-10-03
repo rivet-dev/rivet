5c0e355ce2867ef614f3abe16a90d8422617303d
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-10-03 18:58:56 +0200

    feat: vercel connection frame

--- frontend/src/app/forms/connect-vercel-form.tsx
+++ frontend/src/app/forms/connect-vercel-form.tsx
@@ -1,6 +1,13 @@
+import { faCheck, faSpinnerThird, Icon } from "@rivet-gg/icons";
+import { useQuery } from "@tanstack/react-query";
+import { AnimatePresence, motion } from "framer-motion";
 import { type UseFormReturn, useFormContext } from "react-hook-form";
 import z from "zod";
 import {
+	CodeFrame,
+	CodeGroup,
+	CodePreview,
+	cn,
 	createSchemaForm,
 	FormControl,
 	FormDescription,
@@ -9,6 +16,7 @@ import {
 	FormLabel,
 	FormMessage,
 	Input,
+	ScrollArea,
 	Select,
 	SelectContent,
 	SelectItem,
@@ -56,11 +64,55 @@ export const Plan = ({ className }: { className?: string }) => {
 							</SelectContent>
 						</Select>
 					</FormControl>
-					<FormDescription className="col-span-1"></FormDescription>
+					<FormDescription className="col-span-1">
+						Your Vercel plan determines the configuration required
+						to properly run your Rivet Engine.
+					</FormDescription>
 					<FormMessage className="col-span-1" />
 				</FormItem>
 			)}
 		/>
+	);
+};
+
+const PLAN_TO_MAX_DURATION: Record<string, number> = {
+	hobby: 60,
+	pro: 300,
+	enterprise: 900,
+};
+
+const code = ({ plan }: { plan: string }) =>
+	`{
+	"$schema": "https://openapi.vercel.sh/vercel.json",
+	"fluid": false, 	// [!code highlight]
+	"functions": {
+		"**": {
+			"maxDuration": ${PLAN_TO_MAX_DURATION[plan] || 60}, 	// [!code highlight]
+		},
+	},
+}`;
+
+export const Json = () => {
+	const { watch } = useFormContext<FormValues>();
+
+	const plan = watch("plan");
+	return (
+		<div className="space-y-2 mt-2">
+			<CodeFrame language="json" title="vercel.json">
+				<CodePreview
+					className="w-full min-w-0"
+					language="json"
+					code={code({ plan })}
+				/>
+			</CodeFrame>
+			<FormDescription className="col-span-1">
+				<b>Max Duration</b> - The maximum execution time of your
+				serverless functions.
+				<br />
+				<b>Disable Fluid Compute</b> - Rivet has its own intelligent
+				load balancing mechanism.
+			</FormDescription>
+		</div>
 	);
 };
 
@@ -78,7 +130,6 @@ export const Endpoint = ({ className }: { className?: string }) => {
 					<FormControl className="row-start-2">
 						<Input
 							placeholder="https://your-application.vercel.app"
-							maxLength={25}
 							{...field}
 						/>
 					</FormControl>
@@ -88,3 +139,68 @@ export const Endpoint = ({ className }: { className?: string }) => {
 		/>
 	);
 };
+
+export function ConnectionCheck() {
+	const { watch, formState } = useFormContext<FormValues>();
+	const endpoint = watch("endpoint");
+	const enabled = !!endpoint && z.string().url().safeParse(endpoint).success;
+
+	const { data } = useQuery({
+		queryKey: ["vercel-endpoint-check", endpoint],
+		queryFn: async () => {
+			try {
+				const url = new URL("/health", endpoint);
+				const response = await fetch(url);
+				if (!response.ok) {
+					throw new Error("Failed to connect");
+				}
+				return response.json();
+			} catch {
+				const url = new URL("/api/rivet/health", endpoint);
+				const response = await fetch(url);
+				if (!response.ok) {
+					throw new Error("Failed to connect");
+				}
+				return response.json();
+			}
+		},
+		enabled,
+		refetchInterval: 1000,
+	});
+
+	const success = !!data;
+
+	return (
+		<AnimatePresence>
+			{enabled ? (
+				<motion.div
+					layoutId="msg"
+					className={cn(
+						"text-center text-muted-foreground text-sm overflow-hidden flex items-center justify-center",
+						success && "text-primary-foreground",
+					)}
+					initial={{ height: 0, opacity: 0.5 }}
+					animate={{ height: "4rem", opacity: 1 }}
+				>
+					{success ? (
+						<>
+							<Icon
+								icon={faCheck}
+								className="mr-1.5 text-primary"
+							/>{" "}
+							Runner successfully connected
+						</>
+					) : (
+						<>
+							<Icon
+								icon={faSpinnerThird}
+								className="mr-1.5 animate-spin"
+							/>{" "}
+							Waiting for runner to connect...
+						</>
+					)}
+				</motion.div>
+			) : null}
+		</AnimatePresence>
+	);
+}
