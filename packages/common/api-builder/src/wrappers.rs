711ccbe0ee94ff6db2f88a3783b756395e0a0ee9
MasterPtato <maxpopkov.contact@gmail.com>
2025-10-06 17:00:40 -0700

    fix(api): use custom path extractor

--- packages/common/api-builder/src/wrappers.rs
+++ packages/common/api-builder/src/wrappers.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use axum::{
 	body::Bytes,
-	extract::Path,
 	response::IntoResponse,
 	routing::{
 		delete as axum_delete, get as axum_get, patch as axum_patch, post as axum_post,
@@ -14,7 +13,7 @@ use std::future::Future;
 use crate::{
 	context::ApiCtx,
 	error_response::ApiError,
-	extract::{Extension, Json, Query},
+	extract::{Extension, Json, Path, Query},
 };
 
 /// Macro to generate wrapper functions for HTTP methods
