8c643f189f2605fdddb13523e0753ca7e1364c7d
ABC <79597906+abcxff@users.noreply.github.com>
2025-10-13 17:41:17 -0400

    feat(site): vercel guide (#3171)

--- site/src/data/deploy-options.ts
+++ site/src/data/deploy-options.ts
@@ -1,17 +1,32 @@
+import {
+	faVercel,
+	faRailway,
+	faDocker,
+	faGoogleCloud,
+	faAws,
+    faCloudflare,
+    faServer,
+	faHetzner,
+	faKubernetes,
+} from "@rivet-gg/icons";
+
 export interface DeployOption {
 	title: string;
 	href: string;
+	icon?: any;
 }
 
 export const deployOptions: DeployOption[] = [
 	{
+		title: "Vercel",
+		href: "/docs/deploy/vercel",
+		icon: faVercel,
+	},
+	{
 		title: "Railway",
 		href: "/docs/deploy/railway",
+		icon: faRailway,
 	},
-	// {
-	// 	title: "Vercel",
-	// 	href: "/docs/deploy/vercel",
-	// },
 	// {
 	// 	title: "Freestyle",
 	// 	href: "/docs/deploy/freestyle",
@@ -19,25 +34,31 @@ export const deployOptions: DeployOption[] = [
 	{
 		title: "AWS ECS",
 		href: "/docs/deploy/aws-ecs",
+		icon: faAws,
 	},
 	{
 		title: "Google Cloud Run",
 		href: "/docs/deploy/gcp-cloud-run",
+		icon: faGoogleCloud,
 	},
 	{
 		title: "Kubernetes",
 		href: "/docs/deploy/kubernetes",
+		icon: faKubernetes,
 	},
 	{
 		title: "Hetzner",
 		href: "/docs/deploy/hetzner",
+		icon: faHetzner,
 	},
 	{
 		title: "VM & Bare Metal",
 		href: "/docs/deploy/vm-and-bare-metal",
+		icon: faServer,
 	},
 	{
 		title: "Cloudflare Workers",
 		href: "/docs/deploy/cloudflare-workers",
+		icon: faCloudflare,
 	},
 ];
