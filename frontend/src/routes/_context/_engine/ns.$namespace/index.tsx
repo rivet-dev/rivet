fb4f216342e6de4fe1ab72ba71c9b919defc5fc7
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-10-26 23:48:38 +0100

    fix(fe): redirect engine users to connect page when there's no actors

--- frontend/src/routes/_context/_engine/ns.$namespace/index.tsx
+++ frontend/src/routes/_context/_engine/ns.$namespace/index.tsx
@@ -1,16 +1,24 @@
 import {
 	CatchBoundary,
 	createFileRoute,
+	redirect,
 	useSearch,
 } from "@tanstack/react-router";
 import { Actors } from "@/app/actors";
 import { BuildPrefiller } from "@/app/build-prefiller";
+import { shouldDisplayActors } from "../../_cloud/orgs.$organization/projects.$project/ns.$namespace/index";
 
 export const Route = createFileRoute("/_context/_engine/ns/$namespace/")({
 	component: RouteComponent,
 	beforeLoad: async ({ context }) => {
 		if (context.__type !== "engine") {
 			throw new Error("Invalid context type for this route");
+		}
+
+		const shouldDisplay = await shouldDisplayActors(context);
+
+		if (!shouldDisplay) {
+			throw redirect({ from: Route.to, replace: true, to: "./connect" });
 		}
 	},
 });
@@ -19,15 +27,14 @@ export function RouteComponent() {
 	const { actorId, n } = useSearch({ from: "/_context" });
 
 	return (
-		<>
+		<CatchBoundary getResetKey={() => actorId ?? "no-actor-id"}>
 			<Actors actorId={actorId} />
-
 			<CatchBoundary
 				getResetKey={() => n?.join(",") ?? "no-build-name"}
 				errorComponent={() => null}
 			>
 				{!n ? <BuildPrefiller /> : null}
 			</CatchBoundary>
-		</>
+		</CatchBoundary>
 	);
 }
