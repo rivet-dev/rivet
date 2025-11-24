fac923f7c9d9d226697e06d5bfc64970e44b06ac
NathanFlurry <git@nathanflurry.com>
2025-11-24 08:08:38 +0000

    chore(examples): fix types (#3533)

--- examples/counter-next-js/src/rivet/registry.ts
+++ examples/counter-next-js/src/rivet/registry.ts
@@ -1,11 +1,16 @@
 import { actor, setup } from "rivetkit";
 
-export const counter = actor({
-	state: { count: 0 },
+const counter = actor({
+	state: {
+		count: 0,
+	},
 	actions: {
 		increment: (c, x: number) => {
 			c.state.count += x;
 			c.broadcast("newCount", c.state.count);
+			return c.state.count;
+		},
+		getCount: (c) => {
 			return c.state.count;
 		},
 	},
