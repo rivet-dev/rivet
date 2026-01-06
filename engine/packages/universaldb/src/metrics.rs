1348992b9b0ba4a7c587c089ac71c39af80ca440
NathanFlurry <git@nathanflurry.com>
2026-01-06 03:17:06 +0000

    chore(engine): remove fdb-specific metrics (#3756)

--- engine/packages/universaldb/src/metrics.rs
+++ engine/packages/universaldb/src/metrics.rs
@@ -1,18 +1,6 @@
 use rivet_metrics::{MICRO_BUCKETS, REGISTRY, prometheus::*};
 
 lazy_static::lazy_static! {
-	pub static ref FDB_PING_DURATION: Histogram = register_histogram_with_registry!(
-		"udb_fdb_ping_duration",
-		"Total duration to retrieve a single value from fdb.",
-		MICRO_BUCKETS.to_vec(),
-		*REGISTRY
-	).unwrap();
-	pub static ref FDB_MISSED_PING: IntGauge = register_int_gauge_with_registry!(
-		"udb_fdb_missed_ping",
-		"1 if fdb missed the last ping.",
-		*REGISTRY
-	).unwrap();
-
 	pub static ref KEY_PACK_COUNT: IntCounterVec = register_int_counter_vec_with_registry!(
 		"udb_key_pack_count",
 		"How many times a key has been packed.",
