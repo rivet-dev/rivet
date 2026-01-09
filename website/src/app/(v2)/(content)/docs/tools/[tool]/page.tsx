ea56e118a9f30a69e1d3410059f0d43ae59dca27
Nathan Flurry <developer@nathanflurry.com>
2026-01-08 22:54:30 -0800

    fix(website): fix building on next 16

--- website/src/app/(v2)/(content)/docs/tools/[tool]/page.tsx
+++ website/src/app/(v2)/(content)/docs/tools/[tool]/page.tsx
@@ -1,14 +1,15 @@
 import { redirect } from "next/navigation";
 
 interface PageProps {
-	params: {
+	params: Promise<{
 		tool: string;
-	};
+	}>;
 }
 
-export default function Page({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
+	const { tool } = await params;
 	// HACK: This page allows us to put tools in the sidebar but redirect to a different page. We can't use `href` since that will change which sidebar/tab is active when on the tool's page.
-	redirect(`/docs/${params.tool}`);
+	redirect(`/docs/${tool}`);
 }
 
 export function generateStaticParams() {
