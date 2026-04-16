ac427a6f9197f7069133c8e5451be58ed8573b6e
MasterPtato <maxpopkov.contact@gmail.com>
2026-04-15 19:32:54 -0700

    move to oss: fix: use unique metric name for gateway2 metrics to avoid panic

--- engine/packages/pegboard-gateway2/src/metrics.rs
+++ engine/packages/pegboard-gateway2/src/metrics.rs
@@ -2,7 +2,7 @@ use rivet_metrics::{BUCKETS, REGISTRY, prometheus::*};
 
 lazy_static::lazy_static! {
 	pub static ref TUNNEL_PING_DURATION: Histogram = register_histogram_with_registry!(
-		"gateway_tunnel_ping_duration",
+		"gateway2_tunnel_ping_duration",
 		"RTT of messages from gateway to pegboard.",
 		BUCKETS.to_vec(),
 		*REGISTRY
