a0cebaa55b7f3ba5a3ed46c738f627d6f24fbf03
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2026-04-22 02:11:42 +0200

    fix: frontend build (#4687)
    # Description
    
    Please include a summary of the changes and the related issue. Please also include relevant motivation and context.
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes.
    
    ## Checklist:
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [ ] New and existing unit tests pass locally with my changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import type { Clerk } from "@clerk/clerk-js";
 import { initThirdPartyProviders } from "@/components";
-import { clerkPromise } from "@/lib/auth";
-import { App, createAppRouter } from "./app";
+import { App, router } from "./app";
 
 async function init() {
-	const clerk = await clerkPromise.catch((error) => {
-		console.error("Failed to initialize Clerk", error);
-		return null as unknown as Clerk;
-	});
-	const router = createAppRouter(clerk);
-
 	await initThirdPartyProviders(router, false);
 
 	// biome-ignore lint/style/noNonNullAssertion: it should always be present
