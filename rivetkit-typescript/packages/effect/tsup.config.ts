874fcf05e3ded2e465163869c01a2245ebe9f758
Igor Gassmann <igor@igassmann.me>
2026-04-27 12:18:45 +0200

    chore(effect): fix import path in tsup config

--- rivetkit-typescript/packages/effect/tsup.config.ts
+++ rivetkit-typescript/packages/effect/tsup.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from "tsup";
-import defaultConfig from "../../../tsup.base.ts";
+import defaultConfig from "../../../tsup.base";
 
 export default defineConfig(defaultConfig);
