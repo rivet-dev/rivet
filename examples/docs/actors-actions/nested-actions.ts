2cd54094590d2b68a9ecb1b2b63c80bd0b8082e1
Nathan Flurry <git@nathanflurry.com>
2026-07-21 22:48:44 -0700

    docs(rivetkit): rename nested actions example

--- examples/docs/actors-actions/nested-actions.ts
+++ examples/docs/actors-actions/nested-actions.ts
@@ -3,6 +3,7 @@ import { actor } from "rivetkit";
 const directory = actor({
 	state: { names: [] as string[] },
 	actions: {
+		// Actions can now be nested
 		users: {
 			add: (c, name: string) => {
 				c.state.names.push(name);
