rivet-dev/actors

feat(rivetkit-core): trace queue sends and receives

rivetkit-rust/packages/rivetkit-core/src/actor/internal_storage/mod.rs
@@ -385,18 +385,21 @@ pub(crate) async fn persist_queue_message(
 	id: u64,
 	next_id: u64,
 	message: &PersistedQueueMessage,
+	origin: TraceOrigin,
 ) -> Result<()> {
 	let id = i64::try_from(id).context("queue message id exceeds sqlite integer range")?;
 	let next_id = i64::try_from(next_id).context("queue next id exceeds sqlite integer range")?;
+	let mut params = vec![
+		BindParam::Integer(id),
+		BindParam::Text(message.name.clone()),
+		BindParam::Blob(message.body.clone()),
+		BindParam::Integer(message.created_at),
+	];
+	params.extend(trace_origin_params(origin));
 	db.execute_batch(vec![
 		SqliteBatchStatement {
 			sql: INSERT_QUEUE_MESSAGE_SQL.to_owned(),
-			params: Some(vec![
-				BindParam::Integer(id),
-				BindParam::Text(message.name.clone()),
-				BindParam::Blob(message.body.clone()),
-				BindParam::Integer(message.created_at),
-			]),
+			params: Some(params),
 		},
 		SqliteBatchStatement {
 			sql: UPSERT_QUEUE_NEXT_ID_SQL.to_owned(),
@@ -421,17 +424,18 @@ pub(crate) async fn persist_queue_messages(
 	for chunk in split_queue_tx_chunks(messages) {
 		let mut statements = Vec::with_capacity(chunk.len());
 		for (id, message) in chunk {
+			let mut params = vec![
+				BindParam::Integer(
+					i64::try_from(*id).context("queue message id exceeds sqlite integer range")?,
+				),
+				BindParam::Text(message.name.clone()),
+				BindParam::Blob(message.body.clone()),
+				BindParam::Integer(message.created_at),
+			];
+			params.extend(trace_origin_params(TraceOrigin::default()));
 			statements.push(SqliteBatchStatement {
 				sql: INSERT_QUEUE_MESSAGE_SQL.to_owned(),
-				params: Some(vec![
-					BindParam::Integer(
-						i64::try_from(*id)
-							.context("queue message id exceeds sqlite integer range")?,
-					),
-					BindParam::Text(message.name.clone()),
-					BindParam::Blob(message.body.clone()),
-					BindParam::Integer(message.created_at),
-				]),
+				params: Some(params),
 			});
 		}
 		db.execute_batch(statements)
@@ -645,6 +649,7 @@ fn decode_queue_message_rows(rows: &[Vec<ColumnValue>]) -> Result<Vec<QueueMessa
 					in_flight: None,
 					in_flight_at: None,
 				},
+				trace_origin: read_trace_origin(row, 4, "queue message trace origin")?,
 			})
 		})
 		.collect()
@@ -688,6 +693,7 @@ pub(crate) async fn reset_queue(db: &SqliteDb) -> Result<()> {
 pub(crate) struct QueueMessageRow {
 	pub id: u64,
 	pub message: PersistedQueueMessage,
+	pub trace_origin: TraceOrigin,
 }
 
 pub(crate) async fn user_kv_batch_get(

rivetkit-rust/packages/rivetkit-core/src/actor/internal_storage/queries.rs
@@ -55,11 +55,9 @@ pub(crate) fn claim_one_shots_sql(event_count: usize) -> String {
 pub(crate) const LOAD_QUEUE_NEXT_ID_SQL: &str =
 	"SELECT queue_next_id FROM _rivet_runtime WHERE id = 1";
 pub(crate) const LOAD_QUEUE_STATS_SQL: &str = "SELECT COUNT(*), MAX(id) FROM _rivet_queue";
-pub(crate) const LOAD_QUEUE_MESSAGES_SQL: &str =
-	"SELECT id, name, body, created_at FROM _rivet_queue ORDER BY id";
-pub(crate) const LOAD_QUEUE_MESSAGES_LIMITED_SQL: &str =
-	"SELECT id, name, body, created_at FROM _rivet_queue ORDER BY id LIMIT ?";
-pub(crate) const LOAD_QUEUE_MESSAGES_FOR_NAME_SQL: &str = "SELECT id, name, body, created_at FROM _rivet_queue INDEXED BY _rivet_queue_name_id WHERE name = ? ORDER BY id LIMIT ?";
+pub(crate) const LOAD_QUEUE_MESSAGES_SQL: &str = "SELECT id, name, body, created_at, origin_ray_id, origin_traceparent, origin_tracestate FROM _rivet_queue ORDER BY id";
+pub(crate) const LOAD_QUEUE_MESSAGES_LIMITED_SQL: &str = "SELECT id, name, body, created_at, origin_ray_id, origin_traceparent, origin_tracestate FROM _rivet_queue ORDER BY id LIMIT ?";
+pub(crate) const LOAD_QUEUE_MESSAGES_FOR_NAME_SQL: &str = "SELECT id, name, body, created_at, origin_ray_id, origin_traceparent, origin_tracestate FROM _rivet_queue INDEXED BY _rivet_queue_name_id WHERE name = ? ORDER BY id LIMIT ?";
 pub(crate) const HAS_QUEUE_MESSAGES_SQL: &str = "SELECT 1 FROM _rivet_queue LIMIT 1";
 pub(crate) const HAS_QUEUE_MESSAGES_FOR_NAME_SQL: &str =
 	"SELECT 1 FROM _rivet_queue INDEXED BY _rivet_queue_name_id WHERE name = ? LIMIT 1";
@@ -71,11 +69,10 @@ pub(crate) fn load_queue_messages_by_ids_sql(id_count: usize) -> String {
 		.collect::<Vec<_>>()
 		.join(", ");
 	format!(
-		"SELECT id, name, body, created_at FROM _rivet_queue WHERE id IN ({placeholders}) ORDER BY id"
+		"SELECT id, name, body, created_at, origin_ray_id, origin_traceparent, origin_tracestate FROM _rivet_queue WHERE id IN ({placeholders}) ORDER BY id"
 	)
 }
-pub(crate) const INSERT_QUEUE_MESSAGE_SQL: &str =
-	"INSERT OR REPLACE INTO _rivet_queue (id, name, body, created_at) VALUES (?, ?, ?, ?)";
+pub(crate) const INSERT_QUEUE_MESSAGE_SQL: &str = "INSERT OR REPLACE INTO _rivet_queue (id, name, body, created_at, origin_ray_id, origin_traceparent, origin_tracestate) VALUES (?, ?, ?, ?, ?, ?, ?)";
 pub(crate) const DELETE_QUEUE_MESSAGE_SQL: &str = "DELETE FROM _rivet_queue WHERE id = ?";
 pub(crate) const RESET_QUEUE_SQL: &str = "DELETE FROM _rivet_queue";
 

rivetkit-rust/packages/rivetkit-core/src/telemetry.rs
@@ -13,6 +13,7 @@ use parking_lot::Mutex;
 use tracing_opentelemetry::OpenTelemetrySpanExt as _;
 
 use crate::ActorContext;
+use crate::actor::queue::QueueMessage;
 
 /// Correlation fields accepted at an invocation boundary.
 #[derive(Debug, Default)]
@@ -62,12 +63,34 @@ fn invocation_ray_id(headers: &http::HeaderMap) -> Option<String> {
 /// Name a request invocation is reported under, in place of a caller-supplied path.
 const REQUEST_INVOCATION_NAME: &str = "onRequest";
 
+/// Name a queue send invocation is reported under. The queue itself is an attribute.
+const QUEUE_SEND_INVOCATION_NAME: &str = "queue.send";
+
 /// What an invocation ran, which decides its name and the attributes that
 /// identify it on the span.
 #[derive(Clone, Copy, Debug)]
 enum InvocationSubject<'a> {
 	Action(&'a str),
 	Request { method: &'a str },
+	QueueSend { queue: &'a str },
+}
+
+impl<'a> InvocationSubject<'a> {
+	fn name(self) -> &'a str {
+		match self {
+			Self::Action(name) => name,
+			Self::Request { .. } => REQUEST_INVOCATION_NAME,
+			Self::QueueSend { .. } => QUEUE_SEND_INVOCATION_NAME,
+		}
+	}
+
+	fn record_attributes(self, span: &tracing::Span) {
+		match self {
+			Self::Action(name) => span.record("rivet.action.name", name),
+			Self::Request { method } => span.record("http.request.method", method),
+			Self::QueueSend { queue } => span.record("rivet.queue.name", queue),
+		};
+	}
 }
 
 /// Owns the complete lifecycle of one actor invocation.
@@ -81,6 +104,7 @@ enum InvocationType {
 	Action,
 	Scheduled,
 	Request,
+	QueueSend,
 }
 
 impl InvocationType {
@@ -89,6 +113,7 @@ impl InvocationType {
 			Self::Action => "action",
 			Self::Scheduled => "scheduled",
 			Self::Request => "request",
+			Self::QueueSend => "queue_send",
 		}
 	}
 }
@@ -143,9 +168,9 @@ pub(crate) struct InvocationWorkGuard(ActorInvocationTelemetry);
 /// messages so the work they cause can link back to its origin.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct TraceOrigin {
-	pub(crate) ray_id: Option<String>,
-	pub(crate) traceparent: Option<String>,
-	pub(crate) tracestate: Option<String>,
+	pub ray_id: Option<String>,
+	pub traceparent: Option<String>,
+	pub tracestate: Option<String>,
 }
 
 /// Active actor invocation fields exposed to foreign-runtime adapters.
@@ -243,6 +268,24 @@ impl ActorInvocation {
 		)
 	}
 
+	/// Starts the invocation for one message sent into `queue_name` from
+	/// outside the actor. It ends when the send is acknowledged, or when the
+	/// sender's wait for a completion ends.
+	pub(crate) fn start_queue_send(
+		ctx: &ActorContext,
+		queue_name: &str,
+		incoming: IncomingInvocationContext,
+	) -> Self {
+		Self::start(
+			ctx,
+			InvocationSubject::QueueSend { queue: queue_name },
+			InvocationType::QueueSend,
+			incoming.ray_id,
+			incoming.remote_parent,
+			None,
+		)
+	}
+
 	/// Starts the invocation for one raw HTTP request served by `onRequest`.
 	/// The span is named after the handler rather than the path, because a
 	/// path is caller-supplied and would make the name a cardinality surface.
@@ -272,16 +315,13 @@ impl ActorInvocation {
 		link: Option<SpanContext>,
 	) -> Self {
 		let identity = ctx.telemetry_identity();
-		let (action_name, http_method) = match subject {
-			InvocationSubject::Action(name) => (name, None),
-			InvocationSubject::Request { method } => (REQUEST_INVOCATION_NAME, Some(method)),
-		};
+		let subject_name = subject.name();
 		let span = if tracing::enabled!(target: "rivetkit::telemetry", tracing::Level::INFO) {
 			let span = tracing::info_span!(
 				target: "rivetkit::telemetry",
 				parent: None,
 				"rivet.actor.invoke",
-				otel.name = %format!("{}/{}", identity.actor_name, action_name),
+				otel.name = %format!("{}/{}", identity.actor_name, subject_name),
 				otel.kind = otel_kind(invocation_type),
 				rivet.invocation.type = invocation_type.as_label(),
 				rivet.actor.id = %identity.actor_id,
@@ -291,16 +331,14 @@ impl ActorInvocation {
 				rivet.ray.id = tracing::field::Empty,
 				http.request.method = tracing::field::Empty,
 				http.response.status_code = tracing::field::Empty,
+				rivet.queue.name = tracing::field::Empty,
 				otel.status_code = tracing::field::Empty,
 				error.type = tracing::field::Empty,
 			);
 			if let Some(ray_id) = ray_id.as_deref() {
 				span.record("rivet.ray.id", ray_id);
 			}
-			match http_method {
-				Some(method) => span.record("http.request.method", method),
-				None => span.record("rivet.action.name", action_name),
-			};
+			subject.record_attributes(&span);
 			if let Some(parent) = parent {
 				span.set_parent(Context::new().with_remote_span_context(parent));
 			}
@@ -588,13 +626,59 @@ fn w3c_trace_headers(span_context: &SpanContext) -> OwnedTraceHeaders {
 	headers
 }
 
-/// An action or a raw HTTP request is entered from outside the actor; a
-/// scheduled fire originates inside it.
+/// An action or a raw HTTP request is entered from outside the actor, a
+/// scheduled fire originates inside it, and a queue send produces a message
+/// the actor consumes later.
 fn otel_kind(invocation_type: InvocationType) -> &'static str {
 	match invocation_type {
 		InvocationType::Action | InvocationType::Request => "server",
 		InvocationType::Scheduled => "internal",
+		InvocationType::QueueSend => "producer",
 	}
+}
+
+/// Opens the span covering the moment one queue message is handed to the
+/// actor. It links to the span that sent the message, which is what connects
+/// the consumer's trace to the sender's. Inside an invocation it sits under
+/// that invocation's application span, or the invocation span, and carries its
+/// ray ID. Outside one, as from the run handler, it is a root span carrying the
+/// ray ID the message was sent under. It closes when the caller drops it, which
+/// `try_receive_batch` does as it hands the message back.
+pub(crate) fn start_queue_receive(ctx: &ActorContext, message: &QueueMessage) -> tracing::Span {
+	if !tracing::enabled!(target: "rivetkit::telemetry", tracing::Level::INFO) {
+		return tracing::Span::none();
+	}
+	let identity = ctx.telemetry_identity();
+	let span = tracing::info_span!(
+		target: "rivetkit::telemetry",
+		parent: None,
+		"rivet.queue.receive",
+		otel.name = %format!("{}/queue.receive", identity.actor_name),
+		otel.kind = "consumer",
+		rivet.actor.id = %identity.actor_id,
+		rivet.actor.name = %identity.actor_name,
+		rivet.actor.key = %identity.actor_key,
+		rivet.queue.name = message.name,
+		rivet.ray.id = tracing::field::Empty,
+	);
+	let invocation_parent = ctx
+		.invocation_telemetry()
+		.and_then(|telemetry| telemetry.parent_context().map(|parent| (telemetry, parent)));
+	if let Some((telemetry, parent)) = invocation_parent {
+		if let Some(ray_id) = telemetry.inner.ray_id.as_deref() {
+			span.record("rivet.ray.id", ray_id);
+		}
+		span.set_parent(parent);
+	} else if let Some(ray_id) = &message.trace_origin.ray_id {
+		span.record("rivet.ray.id", ray_id);
+	}
+	if let Some(link) = parse_remote_parent(
+		message.trace_origin.traceparent.as_deref(),
+		message.trace_origin.tracestate.as_deref(),
+	) {
+		span.add_link(link);
+	}
+	span
 }
 
 /// Records the terminal status and error identity of a finished span.

rivet-dev/rivet

rivetkit-rust/packages/rivetkit-core/src/actor/internal_storage/mod.rs
@@ -385,18 +385,21 @@ pub(crate) async fn persist_queue_message(
 	id: u64,
 	next_id: u64,
 	message: &PersistedQueueMessage,
+	trace_context: IncomingTraceContext,
 ) -> Result<()> {
 	let id = i64::try_from(id).context("queue message id exceeds sqlite integer range")?;
 	let next_id = i64::try_from(next_id).context("queue next id exceeds sqlite integer range")?;
+	let mut params = vec![
+		BindParam::Integer(id),
+		BindParam::Text(message.name.clone()),
+		BindParam::Blob(message.body.clone()),
+		BindParam::Integer(message.created_at),
+	];
+	params.extend(trace_context_params(trace_context));
 	db.execute_batch(vec![
 		SqliteBatchStatement {
 			sql: INSERT_QUEUE_MESSAGE_SQL.to_owned(),
-			params: Some(vec![
-				BindParam::Integer(id),
-				BindParam::Text(message.name.clone()),
-				BindParam::Blob(message.body.clone()),
-				BindParam::Integer(message.created_at),
-			]),
+			params: Some(params),
 		},
 		SqliteBatchStatement {
 			sql: UPSERT_QUEUE_NEXT_ID_SQL.to_owned(),
@@ -421,17 +424,18 @@ pub(crate) async fn persist_queue_messages(
 	for chunk in split_queue_tx_chunks(messages) {
 		let mut statements = Vec::with_capacity(chunk.len());
 		for (id, message) in chunk {
+			let mut params = vec![
+				BindParam::Integer(
+					i64::try_from(*id).context("queue message id exceeds sqlite integer range")?,
+				),
+				BindParam::Text(message.name.clone()),
+				BindParam::Blob(message.body.clone()),
+				BindParam::Integer(message.created_at),
+			];
+			params.extend(trace_context_params(IncomingTraceContext::default()));
 			statements.push(SqliteBatchStatement {
 				sql: INSERT_QUEUE_MESSAGE_SQL.to_owned(),
-				params: Some(vec![
-					BindParam::Integer(
-						i64::try_from(*id)
-							.context("queue message id exceeds sqlite integer range")?,
-					),
-					BindParam::Text(message.name.clone()),
-					BindParam::Blob(message.body.clone()),
-					BindParam::Integer(message.created_at),
-				]),
+				params: Some(params),
 			});
 		}
 		db.execute_batch(statements)
@@ -645,6 +649,7 @@ fn decode_queue_message_rows(rows: &[Vec<ColumnValue>]) -> Result<Vec<QueueMessa
 					in_flight: None,
 					in_flight_at: None,
 				},
+				trace_context: read_trace_context(row, 4, "queue message trace context")?,
 			})
 		})
 		.collect()
@@ -688,6 +693,7 @@ pub(crate) async fn reset_queue(db: &SqliteDb) -> Result<()> {
 pub(crate) struct QueueMessageRow {
 	pub id: u64,
 	pub message: PersistedQueueMessage,
+	pub trace_context: IncomingTraceContext,
 }
 
 pub(crate) async fn user_kv_batch_get(

rivetkit-rust/packages/rivetkit-core/src/actor/internal_storage/queries.rs
@@ -55,11 +55,9 @@ pub(crate) fn claim_one_shots_sql(event_count: usize) -> String {
 pub(crate) const LOAD_QUEUE_NEXT_ID_SQL: &str =
 	"SELECT queue_next_id FROM _rivet_runtime WHERE id = 1";
 pub(crate) const LOAD_QUEUE_STATS_SQL: &str = "SELECT COUNT(*), MAX(id) FROM _rivet_queue";
-pub(crate) const LOAD_QUEUE_MESSAGES_SQL: &str =
-	"SELECT id, name, body, created_at FROM _rivet_queue ORDER BY id";
-pub(crate) const LOAD_QUEUE_MESSAGES_LIMITED_SQL: &str =
-	"SELECT id, name, body, created_at FROM _rivet_queue ORDER BY id LIMIT ?";
-pub(crate) const LOAD_QUEUE_MESSAGES_FOR_NAME_SQL: &str = "SELECT id, name, body, created_at FROM _rivet_queue INDEXED BY _rivet_queue_name_id WHERE name = ? ORDER BY id LIMIT ?";
+pub(crate) const LOAD_QUEUE_MESSAGES_SQL: &str = "SELECT id, name, body, created_at, ray_id, traceparent, tracestate FROM _rivet_queue ORDER BY id";
+pub(crate) const LOAD_QUEUE_MESSAGES_LIMITED_SQL: &str = "SELECT id, name, body, created_at, ray_id, traceparent, tracestate FROM _rivet_queue ORDER BY id LIMIT ?";
+pub(crate) const LOAD_QUEUE_MESSAGES_FOR_NAME_SQL: &str = "SELECT id, name, body, created_at, ray_id, traceparent, tracestate FROM _rivet_queue INDEXED BY _rivet_queue_name_id WHERE name = ? ORDER BY id LIMIT ?";
 pub(crate) const HAS_QUEUE_MESSAGES_SQL: &str = "SELECT 1 FROM _rivet_queue LIMIT 1";
 pub(crate) const HAS_QUEUE_MESSAGES_FOR_NAME_SQL: &str =
 	"SELECT 1 FROM _rivet_queue INDEXED BY _rivet_queue_name_id WHERE name = ? LIMIT 1";
@@ -71,11 +69,10 @@ pub(crate) fn load_queue_messages_by_ids_sql(id_count: usize) -> String {
 		.collect::<Vec<_>>()
 		.join(", ");
 	format!(
-		"SELECT id, name, body, created_at FROM _rivet_queue WHERE id IN ({placeholders}) ORDER BY id"
+		"SELECT id, name, body, created_at, ray_id, traceparent, tracestate FROM _rivet_queue WHERE id IN ({placeholders}) ORDER BY id"
 	)
 }
-pub(crate) const INSERT_QUEUE_MESSAGE_SQL: &str =
-	"INSERT OR REPLACE INTO _rivet_queue (id, name, body, created_at) VALUES (?, ?, ?, ?)";
+pub(crate) const INSERT_QUEUE_MESSAGE_SQL: &str = "INSERT OR REPLACE INTO _rivet_queue (id, name, body, created_at, ray_id, traceparent, tracestate) VALUES (?, ?, ?, ?, ?, ?, ?)";
 pub(crate) const DELETE_QUEUE_MESSAGE_SQL: &str = "DELETE FROM _rivet_queue WHERE id = ?";
 pub(crate) const RESET_QUEUE_SQL: &str = "DELETE FROM _rivet_queue";
 

rivetkit-rust/packages/rivetkit-core/src/telemetry.rs
@@ -13,6 +13,7 @@ use parking_lot::Mutex;
 use tracing_opentelemetry::OpenTelemetrySpanExt as _;
 
 use crate::ActorContext;
+use crate::actor::queue::QueueMessage;
 
 /// Correlation fields accepted at an invocation boundary.
 #[derive(Debug, Default)]
@@ -62,12 +63,34 @@ fn invocation_ray_id(headers: &http::HeaderMap) -> Option<String> {
 /// Name a request invocation is reported under, in place of a caller-supplied path.
 const REQUEST_INVOCATION_NAME: &str = "onRequest";
 
+/// Name a queue send invocation is reported under. The queue itself is an attribute.
+const QUEUE_SEND_INVOCATION_NAME: &str = "queue.send";
+
 /// What an invocation ran, which decides its name and the attributes that
 /// identify it on the span.
 #[derive(Clone, Copy, Debug)]
 enum InvocationSubject<'a> {
 	Action(&'a str),
 	Request { method: &'a str },
+	QueueSend { queue: &'a str },
+}
+
+impl<'a> InvocationSubject<'a> {
+	fn name(self) -> &'a str {
+		match self {
+			Self::Action(name) => name,
+			Self::Request { .. } => REQUEST_INVOCATION_NAME,
+			Self::QueueSend { .. } => QUEUE_SEND_INVOCATION_NAME,
+		}
+	}
+
+	fn record_attributes(self, span: &tracing::Span) {
+		match self {
+			Self::Action(name) => span.record("rivet.action.name", name),
+			Self::Request { method } => span.record("http.request.method", method),
+			Self::QueueSend { queue } => span.record("rivet.queue.name", queue),
+		};
+	}
 }
 
 /// Owns the complete lifecycle of one actor invocation.
@@ -81,6 +104,7 @@ enum InvocationType {
 	Action,
 	Scheduled,
 	Request,
+	QueueSend,
 }
 
 impl InvocationType {
@@ -89,6 +113,7 @@ impl InvocationType {
 			Self::Action => "action",
 			Self::Scheduled => "scheduled",
 			Self::Request => "request",
+			Self::QueueSend => "queue_send",
 		}
 	}
 }
@@ -143,9 +168,9 @@ pub(crate) struct InvocationWorkGuard(ActorInvocationTelemetry);
 /// messages so the work they cause can link back to the invocation that caused it.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct IncomingTraceContext {
-	pub(crate) ray_id: Option<String>,
-	pub(crate) traceparent: Option<String>,
-	pub(crate) tracestate: Option<String>,
+	pub ray_id: Option<String>,
+	pub traceparent: Option<String>,
+	pub tracestate: Option<String>,
 }
 
 /// Active actor invocation fields exposed to foreign-runtime adapters.
@@ -245,6 +270,24 @@ impl ActorInvocation {
 		)
 	}
 
+	/// Starts the invocation for one message sent into `queue_name` from
+	/// outside the actor. It ends when the send is acknowledged, or when the
+	/// sender's wait for a completion ends.
+	pub(crate) fn start_queue_send(
+		ctx: &ActorContext,
+		queue_name: &str,
+		incoming: IncomingInvocationContext,
+	) -> Self {
+		Self::start(
+			ctx,
+			InvocationSubject::QueueSend { queue: queue_name },
+			InvocationType::QueueSend,
+			incoming.ray_id,
+			incoming.remote_parent,
+			None,
+		)
+	}
+
 	/// Starts the invocation for one raw HTTP request served by `onRequest`.
 	/// The span is named after the handler rather than the path, because a
 	/// path is caller-supplied and would make the name a cardinality surface.
@@ -274,16 +317,13 @@ impl ActorInvocation {
 		link: Option<SpanContext>,
 	) -> Self {
 		let identity = ctx.telemetry_identity();
-		let (action_name, http_method) = match subject {
-			InvocationSubject::Action(name) => (name, None),
-			InvocationSubject::Request { method } => (REQUEST_INVOCATION_NAME, Some(method)),
-		};
+		let subject_name = subject.name();
 		let span = if tracing::enabled!(target: "rivetkit::telemetry", tracing::Level::INFO) {
 			let span = tracing::info_span!(
 				target: "rivetkit::telemetry",
 				parent: None,
 				"rivet.actor.invoke",
-				otel.name = %format!("{}/{}", identity.actor_name, action_name),
+				otel.name = %format!("{}/{}", identity.actor_name, subject_name),
 				otel.kind = otel_kind(invocation_type),
 				rivet.invocation.type = invocation_type.as_label(),
 				rivet.actor.id = %identity.actor_id,
@@ -293,16 +333,14 @@ impl ActorInvocation {
 				rivet.ray.id = tracing::field::Empty,
 				http.request.method = tracing::field::Empty,
 				http.response.status_code = tracing::field::Empty,
+				rivet.queue.name = tracing::field::Empty,
 				otel.status_code = tracing::field::Empty,
 				error.type = tracing::field::Empty,
 			);
 			if let Some(ray_id) = ray_id.as_deref() {
 				span.record("rivet.ray.id", ray_id);
 			}
-			match http_method {
-				Some(method) => span.record("http.request.method", method),
-				None => span.record("rivet.action.name", action_name),
-			};
+			subject.record_attributes(&span);
 			if let Some(parent) = parent {
 				span.set_parent(Context::new().with_remote_span_context(parent));
 			}
@@ -590,13 +628,59 @@ fn w3c_trace_headers(span_context: &SpanContext) -> OwnedTraceHeaders {
 	headers
 }
 
-/// An action or a raw HTTP request is entered from outside the actor; a
-/// scheduled fire originates inside it.
+/// An action or a raw HTTP request is entered from outside the actor, a
+/// scheduled fire originates inside it, and a queue send produces a message
+/// the actor consumes later.
 fn otel_kind(invocation_type: InvocationType) -> &'static str {
 	match invocation_type {
 		InvocationType::Action | InvocationType::Request => "server",
 		InvocationType::Scheduled => "internal",
+		InvocationType::QueueSend => "producer",
 	}
+}
+
+/// Opens the span covering the moment one queue message is handed to the
+/// actor. It links to the span that sent the message, which is what connects
+/// the consumer's trace to the sender's. Inside an invocation it sits under
+/// that invocation's application span, or the invocation span, and carries its
+/// ray ID. Outside one, as from the run handler, it is a root span carrying the
+/// ray ID the message was sent under. It closes when the caller drops it, which
+/// `try_receive_batch` does as it hands the message back.
+pub(crate) fn start_queue_receive(ctx: &ActorContext, message: &QueueMessage) -> tracing::Span {
+	if !tracing::enabled!(target: "rivetkit::telemetry", tracing::Level::INFO) {
+		return tracing::Span::none();
+	}
+	let identity = ctx.telemetry_identity();
+	let span = tracing::info_span!(
+		target: "rivetkit::telemetry",
+		parent: None,
+		"rivet.queue.receive",
+		otel.name = %format!("{}/queue.receive", identity.actor_name),
+		otel.kind = "consumer",
+		rivet.actor.id = %identity.actor_id,
+		rivet.actor.name = %identity.actor_name,
+		rivet.actor.key = %identity.actor_key,
+		rivet.queue.name = message.name,
+		rivet.ray.id = tracing::field::Empty,
+	);
+	let invocation_parent = ctx
+		.invocation_telemetry()
+		.and_then(|telemetry| telemetry.parent_context().map(|parent| (telemetry, parent)));
+	if let Some((telemetry, parent)) = invocation_parent {
+		if let Some(ray_id) = telemetry.inner.ray_id.as_deref() {
+			span.record("rivet.ray.id", ray_id);
+		}
+		span.set_parent(parent);
+	} else if let Some(ray_id) = &message.trace_context.ray_id {
+		span.record("rivet.ray.id", ray_id);
+	}
+	if let Some(link) = parse_remote_parent(
+		message.trace_context.traceparent.as_deref(),
+		message.trace_context.tracestate.as_deref(),
+	) {
+		span.add_link(link);
+	}
+	span
 }
 
 /// Records the terminal status and error identity of a finished span.