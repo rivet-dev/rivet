rivet-dev/actors

feat(rivetkit): trace actor to actor calls

rivetkit-typescript/packages/rivetkit-napi/index.d.ts
@@ -326,6 +326,12 @@ export declare class ActorContext {
    */
   withApplicationSpan(traceparent?: string | undefined | null, tracestate?: string | undefined | null): ActorContext
   invocationTraceContext(): JsActorInvocationTraceContext | null
+  /**
+   * Opens the span covering one call out to another actor. Returns nothing
+   * when this handle serves no invocation or tracing is disabled, in which
+   * case the caller sends its own context as before.
+   */
+  beginOutboundCall(actorName: string, actionName: string): OutboundCall | null
   provisionActorRuntimeSocket(): Promise<JsActorRuntimeSocketEndpointInfo>
   schedule(): Schedule
   queue(): Queue
@@ -374,6 +380,26 @@ export declare class ActorContext {
   registerTask(promise: Promise<any>): void
   runtimeState(): object
   clearRuntimeState(): void
+}
+/**
+ * One open call out to another actor.
+ *
+ * The call spans a request made by the host runtime, so it is opened and closed
+ * by two separate calls. Letting this be collected without finishing records
+ * the call as cancelled rather than silently losing it.
+ */
+export declare class OutboundCall {
+  /**
+   * W3C context of this call's span, to send to the callee so it parents to
+   * the call rather than to the invocation that made it.
+   */
+  spanContext(): JsActorInvocationSpanContext | null
+  /**
+   * Records the call's outcome. `error` is the failure as the bridge encodes
+   * it, so a structured error keeps its group and code while anything else
+   * stays unstructured for Core to classify.
+   */
+  finish(error?: string | undefined | null): void
 }
 export declare class NapiActorFactory {
   constructor(callbacks: object, config?: JsActorConfig | undefined | null)

rivetkit-typescript/packages/rivetkit/src/registry/native.ts
@@ -4028,6 +4028,10 @@ export function buildNativeFactory(
 					callNativeSync(() =>
 						runtime.actorInvocationTraceContext(ctx),
 					),
+				beginOutboundCall: (actorName, actionName) =>
+					callNativeSync(() =>
+						runtime.beginOutboundCall(ctx, actorName, actionName),
+					),
 			},
 		);
 	const run = getRunFunction(config.run);

rivetkit-typescript/packages/rivetkit/src/registry/runtime.ts
@@ -1,4 +1,7 @@
-import type { ActorInvocationTraceContext } from "@/common/actor-telemetry-context";
+import type {
+	ActorInvocationSpanContext,
+	ActorInvocationTraceContext,
+} from "@/common/actor-telemetry-context";
 import type {
 	SqliteNativeMetrics,
 	SqliteProfilingOptions,
@@ -34,6 +37,30 @@ export interface RuntimeActorKeySegment {
 export type CurrentActorInvocation = () =>
 	| ActorInvocationTraceContext
 	| undefined;
+
+/** One open call from an actor out to another actor. */
+export interface RuntimeOutboundCall {
+	/**
+	 * W3C context of the call's own span, to send to the callee so it parents to
+	 * the call. Absent when tracing is disabled.
+	 */
+	readonly span?: ActorInvocationSpanContext;
+	/**
+	 * Records the call's outcome. `error` is the failure encoded the way the
+	 * bridge encodes errors, so a structured error keeps its group and code.
+	 */
+	finish(error?: string): void;
+}
+
+/**
+ * Opens the span covering one call out to another actor, or returns `undefined`
+ * outside an invocation or on a runtime without invocation telemetry. Callers
+ * send their own context when it returns nothing.
+ */
+export type BeginOutboundCall = (
+	actorName: string,
+	actionName: string,
+) => RuntimeOutboundCall | undefined;
 
 export interface RuntimeHttpRequest {
 	method: string;
@@ -563,6 +590,15 @@ export interface CoreRuntime {
 	actorInvocationTraceContext(
 		ctx: ActorContextHandle,
 	): ActorInvocationTraceContext | undefined;
+	/**
+	 * Opens the span covering one call this actor makes to another actor. See
+	 * `BeginOutboundCall` for when this returns nothing.
+	 */
+	beginOutboundCall(
+		ctx: ActorContextHandle,
+		actorName: string,
+		actionName: string,
+	): RuntimeOutboundCall | undefined;
 	actorName(ctx: ActorContextHandle): string;
 	actorKey(ctx: ActorContextHandle): RuntimeActorKeySegment[];
 	actorRegion(ctx: ActorContextHandle): string;

rivet-dev/rivet

rivetkit-typescript/packages/rivetkit-napi/index.d.ts
@@ -326,6 +326,12 @@ export declare class ActorContext {
    */
   withApplicationSpan(traceparent?: string | undefined | null, tracestate?: string | undefined | null): ActorContext
   invocationTraceContext(): JsActorInvocationTraceContext | null
+  /**
+   * Opens the span covering one call out to another actor. Returns nothing
+   * when this handle serves no invocation or tracing is disabled, in which
+   * case the caller sends its own context as before.
+   */
+  startCallSpan(actorName: string, actionName: string): OutboundCall | null
   provisionActorRuntimeSocket(): Promise<JsActorRuntimeSocketEndpointInfo>
   schedule(): Schedule
   queue(): Queue
@@ -374,6 +380,26 @@ export declare class ActorContext {
   registerTask(promise: Promise<any>): void
   runtimeState(): object
   clearRuntimeState(): void
+}
+/**
+ * One open call out to another actor.
+ *
+ * The call spans a request made by the host runtime, so it is opened and closed
+ * by two separate calls. Letting this be collected without finishing records
+ * the call as cancelled rather than silently losing it.
+ */
+export declare class OutboundCall {
+  /**
+   * W3C context of this call's span, to send to the callee so it parents to
+   * the call rather than to the invocation that made it.
+   */
+  spanContext(): JsActorInvocationSpanContext | null
+  /**
+   * Records the call's outcome. `error` is the failure as the bridge encodes
+   * it, so a structured error keeps its group and code while anything else
+   * stays unstructured for Core to classify.
+   */
+  finish(error?: string | undefined | null): void
 }
 export declare class NapiActorFactory {
   constructor(callbacks: object, config?: JsActorConfig | undefined | null)

rivetkit-typescript/packages/rivetkit/src/registry/native.ts
@@ -4028,6 +4028,10 @@ export function buildNativeFactory(
 					callNativeSync(() =>
 						runtime.actorInvocationTraceContext(ctx),
 					),
+				startCallSpan: (actorName, actionName) =>
+					callNativeSync(() =>
+						runtime.startCallSpan(ctx, actorName, actionName),
+					),
 			},
 		);
 	const run = getRunFunction(config.run);

rivetkit-typescript/packages/rivetkit/src/registry/runtime.ts
@@ -1,4 +1,7 @@
-import type { ActorInvocationTraceContext } from "@/common/actor-telemetry-context";
+import type {
+	ActorInvocationSpanContext,
+	ActorInvocationTraceContext,
+} from "@/common/actor-telemetry-context";
 import type {
 	SqliteNativeMetrics,
 	SqliteProfilingOptions,
@@ -34,6 +37,30 @@ export interface RuntimeActorKeySegment {
 export type CurrentActorInvocation = () =>
 	| ActorInvocationTraceContext
 	| undefined;
+
+/** One open call from an actor out to another actor. */
+export interface RuntimeOutboundCall {
+	/**
+	 * W3C context of the call's own span, to send to the callee so it parents to
+	 * the call. Absent when tracing is disabled.
+	 */
+	readonly span?: ActorInvocationSpanContext;
+	/**
+	 * Records the call's outcome. `error` is the failure encoded the way the
+	 * bridge encodes errors, so a structured error keeps its group and code.
+	 */
+	finish(error?: string): void;
+}
+
+/**
+ * Opens the span covering one call out to another actor, or returns `undefined`
+ * outside an invocation or on a runtime without invocation telemetry. Callers
+ * send their own context when it returns nothing.
+ */
+export type StartCallSpan = (
+	actorName: string,
+	actionName: string,
+) => RuntimeOutboundCall | undefined;
 
 export interface RuntimeHttpRequest {
 	method: string;
@@ -563,6 +590,15 @@ export interface CoreRuntime {
 	actorInvocationTraceContext(
 		ctx: ActorContextHandle,
 	): ActorInvocationTraceContext | undefined;
+	/**
+	 * Opens the span covering one call this actor makes to another actor. See
+	 * `StartCallSpan` for when this returns nothing.
+	 */
+	startCallSpan(
+		ctx: ActorContextHandle,
+		actorName: string,
+		actionName: string,
+	): RuntimeOutboundCall | undefined;
 	actorName(ctx: ActorContextHandle): string;
 	actorKey(ctx: ActorContextHandle): RuntimeActorKeySegment[];
 	actorRegion(ctx: ActorContextHandle): string;