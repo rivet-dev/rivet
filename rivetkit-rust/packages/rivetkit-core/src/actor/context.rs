rivet-dev/actors

feat(rivetkit-core): trace scheduled invocations

rivetkit-rust/packages/rivetkit-core/src/actor/context.rs
@@ -260,6 +260,28 @@ impl ActorContext {
 		self
 	}
 
+	/// Returns a handle for the same invocation whose spans parent to the
+	/// application span the host runtime has active, given as W3C
+	/// `traceparent` and `tracestate`. A handle that serves no invocation is
+	/// returned unchanged, because it opens no spans.
+	#[doc(hidden)]
+	pub fn with_application_span(
+		&self,
+		traceparent: Option<&str>,
+		tracestate: Option<&str>,
+	) -> Self {
+		Self(
+			self.0.clone(),
+			self.1
+				.as_ref()
+				.map(|telemetry| telemetry.with_application_span(traceparent, tracestate)),
+		)
+	}
+
+	pub(crate) fn invocation_telemetry(&self) -> Option<&ActorInvocationTelemetry> {
+		self.1.as_ref()
+	}
+
 	/// Returns the SQLite handle bound to this handle's invocation.
 	pub fn invocation_sql(&self) -> SqliteDb {
 		self.0.sql.clone().with_invocation_telemetry(self.1.clone())
@@ -1813,8 +1835,14 @@ impl ActorContext {
 		self.track_shutdown_task(async move {
 			let _internal_keep_awake_region = internal_keep_awake_region;
 			ctx.record_user_task_started(UserTaskKind::ScheduledAction);
-			let started_at = Instant::now();
+			let user_task_started_at = Instant::now();
 			let action_name = action.clone();
+			let invocation = crate::telemetry::ActorInvocation::start_scheduled(
+				&ctx,
+				&action_name,
+				dispatch.origin,
+			);
+			let invocation_telemetry = invocation.telemetry();
 			let (reply_tx, reply_rx) = oneshot::channel();
 
 			let mut dispatch_error = None;
@@ -1824,7 +1852,7 @@ impl ActorContext {
 					args,
 					conn: None,
 					scheduled_fire: Some(scheduled_fire),
-					invocation_telemetry: None,
+					invocation_telemetry: Some(invocation_telemetry),
 					reply: Reply::from(reply_tx),
 				},
 				"scheduled_action",
@@ -1840,8 +1868,8 @@ impl ActorContext {
 							"scheduled event execution failed"
 						);
 					}
-					Err(error) => {
-						dispatch_error = Some(error.into());
+					Err(_) => {
+						dispatch_error = Some(ActorLifecycleError::DroppedReply.build());
 						tracing::error!(
 							error = ?dispatch_error.as_ref().expect("just assigned"),
 							event_id,
@@ -1860,6 +1888,8 @@ impl ActorContext {
 					);
 				}
 			}
+			invocation.finish(dispatch_error.as_ref());
+
 			ctx.finish_schedule_dispatch(&event_id, history_id, dispatch_error.as_ref())
 				.await;
 			if let (Some(name), Some(error)) = (recurring_name, dispatch_error.as_ref()) {
@@ -1876,7 +1906,10 @@ impl ActorContext {
 				}
 			}
 
-			ctx.record_user_task_finished(UserTaskKind::ScheduledAction, started_at.elapsed());
+			ctx.record_user_task_finished(
+				UserTaskKind::ScheduledAction,
+				user_task_started_at.elapsed(),
+			);
 		});
 	}
 

rivetkit-rust/packages/rivetkit-core/src/actor/internal_storage/schema.rs
@@ -3,7 +3,7 @@ use anyhow::{Context, Result, bail};
 use super::queries::{LOAD_META_TEXT_SQL, UPSERT_META_TEXT_SQL};
 use crate::sqlite::{BindParam, ColumnValue, SqliteBatchStatement, SqliteDb};
 
-pub(crate) const INTERNAL_SCHEMA_VERSION: i64 = 1;
+pub(crate) const INTERNAL_SCHEMA_VERSION: i64 = 2;
 
 const SCHEMA_VERSION_KEY: &str = "schema_version";
 
@@ -27,7 +27,9 @@ CREATE TABLE IF NOT EXISTS _rivet_meta (
 // across runtime releases. Rewriting these entries in place is safe only while
 // no internal schema version has shipped; after release, all changes must be
 // appended as new migrations and INTERNAL_SCHEMA_VERSION must advance.
-pub(crate) const MIGRATIONS: &[&[&str]] = &[&[
+pub(crate) const MIGRATIONS: &[&[&str]] = &[MIGRATION_V1, MIGRATION_V2];
+
+const MIGRATION_V1: &[&str] = &[
 	// W[queue_next_id per enqueue; alarm per head-change; token once | point UPDATE of one column | <100 B | single-row: all runtime singletons on one leaf]
 	r#"
 CREATE TABLE _rivet_runtime (
@@ -153,7 +155,16 @@ CREATE TABLE _rivet_user_kv (
     value BLOB NOT NULL
 ) STRICT, WITHOUT ROWID
 	"#,
-]];
+];
+
+const MIGRATION_V2: &[&str] = &[
+	"ALTER TABLE _rivet_schedule_events ADD COLUMN origin_ray_id TEXT",
+	"ALTER TABLE _rivet_schedule_events ADD COLUMN origin_traceparent TEXT",
+	"ALTER TABLE _rivet_schedule_events ADD COLUMN origin_tracestate TEXT",
+	"ALTER TABLE _rivet_queue ADD COLUMN origin_ray_id TEXT",
+	"ALTER TABLE _rivet_queue ADD COLUMN origin_traceparent TEXT",
+	"ALTER TABLE _rivet_queue ADD COLUMN origin_tracestate TEXT",
+];
 
 pub(crate) async fn ensure_internal_schema(db: &SqliteDb) -> Result<()> {
 	db.execute(CREATE_META_TABLE, None)

rivetkit-rust/packages/rivetkit-core/tests/internal_schema.rs
@@ -60,13 +60,80 @@ fn unpublished_schema_has_explicit_values_and_minimal_constraints() {
 }
 
 #[test]
-fn logical_run_wake_metadata_keeps_the_v1_schema_openable() {
+fn v1_database_upgrades_to_the_current_schema() {
+	let conn = rusqlite::Connection::open_in_memory().expect("open fixture database");
+	conn.execute_batch(CREATE_META_TABLE).unwrap();
+	for sql in MIGRATIONS[0] {
+		conn.execute_batch(sql).unwrap();
+	}
+	conn.execute(
+		UPSERT_META_TEXT_SQL,
+		rusqlite::params![SCHEMA_VERSION_KEY, encode_schema_version(1)],
+	)
+	.unwrap();
+	conn.execute(
+		"INSERT INTO _rivet_schedule_events (event_id, trigger_at, action, args, kind, cron_expression, timezone, interval_ms, last_started_at, max_history) VALUES ('at:1', 5, 'tick', NULL, 0, NULL, NULL, NULL, NULL, 0)",
+		[],
+	)
+	.unwrap();
+	conn.execute(
+		"INSERT INTO _rivet_queue (id, name, body, created_at) VALUES (1, 'jobs', X'00', 5)",
+		[],
+	)
+	.unwrap();
+
+	for statement in migration_statements(1, INTERNAL_SCHEMA_VERSION).unwrap() {
+		let params = statement
+			.params
+			.unwrap_or_default()
+			.into_iter()
+			.map(|param| match param {
+				BindParam::Text(text) => rusqlite::types::Value::Text(text),
+				BindParam::Blob(blob) => rusqlite::types::Value::Blob(blob),
+				BindParam::Integer(value) => rusqlite::types::Value::Integer(value),
+				other => panic!("unexpected migration bind parameter: {other:?}"),
+			})
+			.collect::<Vec<_>>();
+		conn.execute(&statement.sql, rusqlite::params_from_iter(params))
+			.unwrap_or_else(|error| panic!("apply migration {}: {error}", statement.sql));
+	}
+
+	let stored_schema: Vec<u8> = conn
+		.query_row(
+			LOAD_META_TEXT_SQL,
+			rusqlite::params![SCHEMA_VERSION_KEY],
+			|row| row.get(0),
+		)
+		.unwrap();
+	assert_eq!(
+		decode_schema_version(&stored_schema).unwrap(),
+		INTERNAL_SCHEMA_VERSION
+	);
+	let origin: (Option<String>, Option<String>, Option<String>) = conn
+		.query_row(
+			"SELECT origin_ray_id, origin_traceparent, origin_tracestate FROM _rivet_schedule_events WHERE event_id = 'at:1'",
+			[],
+			|row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
+		)
+		.unwrap();
+	assert_eq!(origin, (None, None, None));
+	let origin: (Option<String>, Option<String>, Option<String>) = conn
+		.query_row(
+			"SELECT origin_ray_id, origin_traceparent, origin_tracestate FROM _rivet_queue WHERE id = 1",
+			[],
+			|row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
+		)
+		.unwrap();
+	assert_eq!(origin, (None, None, None));
+}
+
+#[test]
+fn logical_run_wake_metadata_lives_in_the_meta_table() {
 	use rivetkit_actor_persist::versioned::RunWakeAt;
 	use vbare::OwnedVersionedData;
 
-	assert_eq!(INTERNAL_SCHEMA_VERSION, 1);
-	let conn = rusqlite::Connection::open_in_memory().expect("open v1 fixture database");
-	initialize_test_schema(&conn).expect("initialize v1 actor schema");
+	let conn = rusqlite::Connection::open_in_memory().expect("open fixture database");
+	initialize_test_schema(&conn).expect("initialize actor schema");
 	let logical_wake = RunWakeAt::wrap_latest(Some(1_723_456_789_000))
 		.serialize_with_embedded_version(1)
 		.expect("encode logical run wake");
@@ -78,28 +145,6 @@ fn logical_run_wake_metadata_keeps_the_v1_schema_openable() {
 		],
 	)
 	.expect("persist reserved metadata row");
-	conn.execute(
-		"INSERT INTO _rivet_runtime (id, last_pushed_alarm, inspector_token, queue_next_id) VALUES (1, ?1, NULL, 2)",
-		rusqlite::params![1_723_456_789_500_i64],
-	)
-	.expect("persist v1 runtime row");
-
-	let stored_schema: Vec<u8> = conn
-		.query_row(
-			LOAD_META_TEXT_SQL,
-			rusqlite::params![SCHEMA_VERSION_KEY],
-			|row| row.get(0),
-		)
-		.expect("read schema version as an old runtime would");
-	assert_eq!(decode_schema_version(&stored_schema).unwrap(), 1);
-	let legacy_runtime: (Option<i64>, Option<String>, i64) = conn
-		.query_row(
-			"SELECT last_pushed_alarm, inspector_token, queue_next_id FROM _rivet_runtime WHERE id = 1",
-			[],
-			|row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
-		)
-		.expect("open runtime through the v1 projection");
-	assert_eq!(legacy_runtime, (Some(1_723_456_789_500), None, 2));
 	let stored_wake: Vec<u8> = conn
 		.query_row(
 			LOAD_META_TEXT_SQL,

rivet-dev/rivet

rivetkit-rust/packages/rivetkit-core/src/actor/context.rs
@@ -260,6 +260,28 @@ impl ActorContext {
 		self
 	}
 
+	/// Returns a handle for the same invocation whose spans parent to the
+	/// application span the host runtime has active, given as W3C
+	/// `traceparent` and `tracestate`. A handle that serves no invocation is
+	/// returned unchanged, because it opens no spans.
+	#[doc(hidden)]
+	pub fn with_application_span(
+		&self,
+		traceparent: Option<&str>,
+		tracestate: Option<&str>,
+	) -> Self {
+		Self(
+			self.0.clone(),
+			self.1
+				.as_ref()
+				.map(|telemetry| telemetry.with_application_span(traceparent, tracestate)),
+		)
+	}
+
+	pub(crate) fn invocation_telemetry(&self) -> Option<&ActorInvocationTelemetry> {
+		self.1.as_ref()
+	}
+
 	/// Returns the SQLite handle bound to this handle's invocation.
 	pub fn invocation_sql(&self) -> SqliteDb {
 		self.0.sql.clone().with_invocation_telemetry(self.1.clone())
@@ -1813,8 +1835,14 @@ impl ActorContext {
 		self.track_shutdown_task(async move {
 			let _internal_keep_awake_region = internal_keep_awake_region;
 			ctx.record_user_task_started(UserTaskKind::ScheduledAction);
-			let started_at = Instant::now();
+			let user_task_started_at = Instant::now();
 			let action_name = action.clone();
+			let invocation = crate::telemetry::ActorInvocation::start_scheduled(
+				&ctx,
+				&action_name,
+				dispatch.trace_context,
+			);
+			let invocation_telemetry = invocation.telemetry();
 			let (reply_tx, reply_rx) = oneshot::channel();
 
 			let mut dispatch_error = None;
@@ -1824,7 +1852,7 @@ impl ActorContext {
 					args,
 					conn: None,
 					scheduled_fire: Some(scheduled_fire),
-					invocation_telemetry: None,
+					invocation_telemetry: Some(invocation_telemetry),
 					reply: Reply::from(reply_tx),
 				},
 				"scheduled_action",
@@ -1840,8 +1868,8 @@ impl ActorContext {
 							"scheduled event execution failed"
 						);
 					}
-					Err(error) => {
-						dispatch_error = Some(error.into());
+					Err(_) => {
+						dispatch_error = Some(ActorLifecycleError::DroppedReply.build());
 						tracing::error!(
 							error = ?dispatch_error.as_ref().expect("just assigned"),
 							event_id,
@@ -1860,6 +1888,8 @@ impl ActorContext {
 					);
 				}
 			}
+			invocation.finish(dispatch_error.as_ref());
+
 			ctx.finish_schedule_dispatch(&event_id, history_id, dispatch_error.as_ref())
 				.await;
 			if let (Some(name), Some(error)) = (recurring_name, dispatch_error.as_ref()) {
@@ -1876,7 +1906,10 @@ impl ActorContext {
 				}
 			}
 
-			ctx.record_user_task_finished(UserTaskKind::ScheduledAction, started_at.elapsed());
+			ctx.record_user_task_finished(
+				UserTaskKind::ScheduledAction,
+				user_task_started_at.elapsed(),
+			);
 		});
 	}
 

rivetkit-rust/packages/rivetkit-core/src/actor/internal_storage/schema.rs
@@ -3,7 +3,7 @@ use anyhow::{Context, Result, bail};
 use super::queries::{LOAD_META_TEXT_SQL, UPSERT_META_TEXT_SQL};
 use crate::sqlite::{BindParam, ColumnValue, SqliteBatchStatement, SqliteDb};
 
-pub(crate) const INTERNAL_SCHEMA_VERSION: i64 = 1;
+pub(crate) const INTERNAL_SCHEMA_VERSION: i64 = 2;
 
 const SCHEMA_VERSION_KEY: &str = "schema_version";
 
@@ -27,7 +27,9 @@ CREATE TABLE IF NOT EXISTS _rivet_meta (
 // across runtime releases. Rewriting these entries in place is safe only while
 // no internal schema version has shipped; after release, all changes must be
 // appended as new migrations and INTERNAL_SCHEMA_VERSION must advance.
-pub(crate) const MIGRATIONS: &[&[&str]] = &[&[
+pub(crate) const MIGRATIONS: &[&[&str]] = &[MIGRATION_V1, MIGRATION_V2];
+
+const MIGRATION_V1: &[&str] = &[
 	// W[queue_next_id per enqueue; alarm per head-change; token once | point UPDATE of one column | <100 B | single-row: all runtime singletons on one leaf]
 	r#"
 CREATE TABLE _rivet_runtime (
@@ -153,7 +155,16 @@ CREATE TABLE _rivet_user_kv (
     value BLOB NOT NULL
 ) STRICT, WITHOUT ROWID
 	"#,
-]];
+];
+
+const MIGRATION_V2: &[&str] = &[
+	"ALTER TABLE _rivet_schedule_events ADD COLUMN ray_id TEXT",
+	"ALTER TABLE _rivet_schedule_events ADD COLUMN traceparent TEXT",
+	"ALTER TABLE _rivet_schedule_events ADD COLUMN tracestate TEXT",
+	"ALTER TABLE _rivet_queue ADD COLUMN ray_id TEXT",
+	"ALTER TABLE _rivet_queue ADD COLUMN traceparent TEXT",
+	"ALTER TABLE _rivet_queue ADD COLUMN tracestate TEXT",
+];
 
 pub(crate) async fn ensure_internal_schema(db: &SqliteDb) -> Result<()> {
 	db.execute(CREATE_META_TABLE, None)

rivetkit-rust/packages/rivetkit-core/tests/internal_schema.rs
@@ -60,13 +60,80 @@ fn unpublished_schema_has_explicit_values_and_minimal_constraints() {
 }
 
 #[test]
-fn logical_run_wake_metadata_keeps_the_v1_schema_openable() {
+fn v1_database_upgrades_to_the_current_schema() {
+	let conn = rusqlite::Connection::open_in_memory().expect("open fixture database");
+	conn.execute_batch(CREATE_META_TABLE).unwrap();
+	for sql in MIGRATIONS[0] {
+		conn.execute_batch(sql).unwrap();
+	}
+	conn.execute(
+		UPSERT_META_TEXT_SQL,
+		rusqlite::params![SCHEMA_VERSION_KEY, encode_schema_version(1)],
+	)
+	.unwrap();
+	conn.execute(
+		"INSERT INTO _rivet_schedule_events (event_id, trigger_at, action, args, kind, cron_expression, timezone, interval_ms, last_started_at, max_history) VALUES ('at:1', 5, 'tick', NULL, 0, NULL, NULL, NULL, NULL, 0)",
+		[],
+	)
+	.unwrap();
+	conn.execute(
+		"INSERT INTO _rivet_queue (id, name, body, created_at) VALUES (1, 'jobs', X'00', 5)",
+		[],
+	)
+	.unwrap();
+
+	for statement in migration_statements(1, INTERNAL_SCHEMA_VERSION).unwrap() {
+		let params = statement
+			.params
+			.unwrap_or_default()
+			.into_iter()
+			.map(|param| match param {
+				BindParam::Text(text) => rusqlite::types::Value::Text(text),
+				BindParam::Blob(blob) => rusqlite::types::Value::Blob(blob),
+				BindParam::Integer(value) => rusqlite::types::Value::Integer(value),
+				other => panic!("unexpected migration bind parameter: {other:?}"),
+			})
+			.collect::<Vec<_>>();
+		conn.execute(&statement.sql, rusqlite::params_from_iter(params))
+			.unwrap_or_else(|error| panic!("apply migration {}: {error}", statement.sql));
+	}
+
+	let stored_schema: Vec<u8> = conn
+		.query_row(
+			LOAD_META_TEXT_SQL,
+			rusqlite::params![SCHEMA_VERSION_KEY],
+			|row| row.get(0),
+		)
+		.unwrap();
+	assert_eq!(
+		decode_schema_version(&stored_schema).unwrap(),
+		INTERNAL_SCHEMA_VERSION
+	);
+	let trace_context: (Option<String>, Option<String>, Option<String>) = conn
+		.query_row(
+			"SELECT ray_id, traceparent, tracestate FROM _rivet_schedule_events WHERE event_id = 'at:1'",
+			[],
+			|row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
+		)
+		.unwrap();
+	assert_eq!(trace_context, (None, None, None));
+	let trace_context: (Option<String>, Option<String>, Option<String>) = conn
+		.query_row(
+			"SELECT ray_id, traceparent, tracestate FROM _rivet_queue WHERE id = 1",
+			[],
+			|row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
+		)
+		.unwrap();
+	assert_eq!(trace_context, (None, None, None));
+}
+
+#[test]
+fn logical_run_wake_metadata_lives_in_the_meta_table() {
 	use rivetkit_actor_persist::versioned::RunWakeAt;
 	use vbare::OwnedVersionedData;
 
-	assert_eq!(INTERNAL_SCHEMA_VERSION, 1);
-	let conn = rusqlite::Connection::open_in_memory().expect("open v1 fixture database");
-	initialize_test_schema(&conn).expect("initialize v1 actor schema");
+	let conn = rusqlite::Connection::open_in_memory().expect("open fixture database");
+	initialize_test_schema(&conn).expect("initialize actor schema");
 	let logical_wake = RunWakeAt::wrap_latest(Some(1_723_456_789_000))
 		.serialize_with_embedded_version(1)
 		.expect("encode logical run wake");
@@ -78,28 +145,6 @@ fn logical_run_wake_metadata_keeps_the_v1_schema_openable() {
 		],
 	)
 	.expect("persist reserved metadata row");
-	conn.execute(
-		"INSERT INTO _rivet_runtime (id, last_pushed_alarm, inspector_token, queue_next_id) VALUES (1, ?1, NULL, 2)",
-		rusqlite::params![1_723_456_789_500_i64],
-	)
-	.expect("persist v1 runtime row");
-
-	let stored_schema: Vec<u8> = conn
-		.query_row(
-			LOAD_META_TEXT_SQL,
-			rusqlite::params![SCHEMA_VERSION_KEY],
-			|row| row.get(0),
-		)
-		.expect("read schema version as an old runtime would");
-	assert_eq!(decode_schema_version(&stored_schema).unwrap(), 1);
-	let legacy_runtime: (Option<i64>, Option<String>, i64) = conn
-		.query_row(
-			"SELECT last_pushed_alarm, inspector_token, queue_next_id FROM _rivet_runtime WHERE id = 1",
-			[],
-			|row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
-		)
-		.expect("open runtime through the v1 projection");
-	assert_eq!(legacy_runtime, (Some(1_723_456_789_500), None, 2));
 	let stored_wake: Vec<u8> = conn
 		.query_row(
 			LOAD_META_TEXT_SQL,